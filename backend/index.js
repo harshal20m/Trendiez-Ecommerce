7dd1fb8840e9731518c94ece1802569e99d77ece
harshal mali <20harshalmali@gmail.com>
2024-10-31 22:26:26 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -269,7 +269,7 @@ app.post("/removefromcart", fetchUser, async (req, res) => {
 app.post("/getcart", fetchUser, async (req, res) => {
 	console.log("GetCart");
 	let userData = await Users.findOne({ _id: req.user.id });
-	res.json(userData.cartData);
+	// res.json(userData.cartData);
 });
 
 app.listen(port, (error) => {
